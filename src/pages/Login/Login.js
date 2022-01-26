da0a3089e8897d0c6d906835d35b13f881b0360a
jpsm83 <jpsm83@hotmail.com>
2022-01-26 09:05:17 +0100

    signup page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
+import Signup from "../Signup/Signup";
 
 export default function Login() {
+
+const [signin, setSignin] = useState(false);
+const [getStarted, setGetStarted] = useState(false);
+
   return (
     <div className="loginScreen">
       <div className="loginScreeBackground">
@@ -10,21 +15,25 @@ export default function Login() {
           src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt="Netflix Logo"
         />
-        <button className="logginScreenButton">Sign In</button>
+        <button className="logginScreenButton" onClick={() => setSignin(true)}>Sign In</button>
       <div className="loginScreenGradient" />
       </div>
       <div className="logingScreenBody">
-          <>
+      {signin ? (
+        <Signup />
+      ) : (
+        <>
               <h1>Unlimited films, TV programmes and more.</h1>
               <h2>Watch anywhere. Cancel at any time.</h2>
               <h3>Ready to watch? Enter your email to create or restart our membership.</h3>
               <div className="loginScreenInput">
                 <form>
                     <input type="email" placeholder="Email Address" />
-                    <button className="loginScreenGetStarted">GET STARTED</button>
+                    <button className="loginScreenGetStarted" onClick={() => setGetStarted(true)}>GET STARTED</button>
                 </form>
               </div>
           </>
+      )}
       </div>
     </div>
   );
